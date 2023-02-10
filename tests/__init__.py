bdaf664d8bcfe87d7a60799864f5fcd987bc199e
timsankara <chimpy.the.dev@outlook.com>
2023-02-11 01:10:52 +0300

    remove print statement in tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,4 @@ import pathlib
 
 if __name__ == "__main__":
     for file in pathlib.Path(__file__).parent.glob("recorded/**/*.txt"):
-        print("FILE NAME: ", file)
         test_saved_examples(filename=file, min_version=(3, 7))
