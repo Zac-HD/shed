4441f8d171d08d45d50e5dffdb072c8e8b9a861c
Rudyard Richter <richter3141@gmail.com>
2023-02-27 16:22:29 -0700

    Add refactors for builtin constructors
    - Replace builtins with literal values
    - Replace empty list constructions with []
    
    Completes a couple tasks in #22.

--- src/shed/_codemods.py
+++ src/shed/_codemods.py
@@ -176,6 +176,40 @@ class ShedFixers(VisitorBasedCodemodCommand):
         except SyntaxError:
             return updated_node
 
+    @m.leave(
+        m.Call(func=m.Name("dict"), args=[])
+        | m.Call(func=m.Name("list"), args=[])
+        | m.Call(func=m.Name("tuple"), args=[])
+    )
+    def replace_builtin_with_literal(self, _, updated_node):
+        if updated_node.func.value == "dict":
+            return cst.Dict([])
+        elif updated_node.func.value == "list":
+            return cst.List([])
+        elif updated_node.func.value == "tuple":
+            return cst.Tuple([])
+        else:
+            raise AssertionError("unreachable")
+
+    @m.leave(
+        m.Call(
+            func=m.Name("list"),
+            args=[m.Arg(m.Dict([]) | m.List([]) | m.SimpleString() | m.Tuple([]))],
+        )
+    )
+    def replace_builtin_empty_list(self, _, updated_node):
+        val = updated_node.args[0].value
+        val_is_empty_seq = (
+            isinstance(val, (cst.Dict, cst.List, cst.Tuple)) and not val.elements
+        )
+        val_is_empty_str = (
+            isinstance(val, cst.SimpleString) and val.evaluated_value == ""
+        )
+        if val_is_empty_seq or val_is_empty_str:
+            return cst.List([])
+        else:
+            return updated_node
+
     # The following methods fix https://pypi.org/project/flake8-comprehensions/
 
     @m.leave(m.Call(func=m.Name("list"), args=[m.Arg(m.GeneratorExp())]))
